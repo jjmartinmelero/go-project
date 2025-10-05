aaa65c175ca978f1cd93da3a137507fb9352d242
jjmartinmelero <juanjesusmelero@hotmail.com>
2025-10-05 12:11:31 +0200

    finished project 5.1

--- 05.1Project/main.go
+++ 05.1Project/main.go
@@ -1,8 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 type Task struct {
 	name         string
-	descriptions string
+	description string
 	complete     bool
 }
 
@@ -32,4 +38,76 @@ func (tl *TaskList) deleteTask(index int) {
 
 func main() {
 
+	// Instance task list
+	list := TaskList{}
+	read := bufio.NewReader(os.Stdin)
+	
+
+	for {
+		var option int
+		fmt.Println("Choose an option: \n",
+			"1. Add Task \n",
+			"2. Mark Task as Complete \n",
+			"3. Edit Task \n",
+			"4. Delete Task \n",
+			"5. Exist \n",)
+		
+		fmt.Println("Select an option: ")
+		fmt.Scanln(&option)
+
+		switch option {
+		case 1: 
+			var task Task
+			fmt.Println("Task name: ")
+			task.name, _ = read.ReadString('\n')
+			
+			fmt.Println("Task description: ")
+			task.description, _ = read.ReadString('\n')
+
+			list.addTask(task)
+			fmt.Println("Task added successfully !")
+		case 2: 
+			var index int
+			fmt.Println("Task index to mark as complete: ")
+			fmt.Scanln(&index)
+			list.markAsComplete(index)
+			fmt.Println("Task marked as complete !")
+		case 3: 
+			var index int
+			var task Task
+			fmt.Println("Task index to edit: ")
+			fmt.Scanln(&index)
+
+			fmt.Println("Task name: ")
+			task.name, _ = read.ReadString('\n')
+			
+			fmt.Println("Task description: ")
+			task.description, _ = read.ReadString('\n')
+
+			list.editTask(index, task)
+
+		case 4: 
+			var index int
+			fmt.Println("Task index to delete: ")
+			fmt.Scanln(&index)
+
+			list.deleteTask(index)
+			fmt.Println("Task deleted successfully !")
+
+		case 5: 
+			fmt.Println("Goodbye !")
+			return
+
+		default: 
+			fmt.Println("Invalid option !")
+		}
+
+		fmt.Println("Task list :")
+		fmt.Println("==============================================")
+		
+		for i, task := range list.tasks {
+			fmt.Printf("%d. - %s - %s Complete: %t\n", i, task.name, task.description, task.complete)
+		}
+		fmt.Println(("=============================================="))
+	}
 }
